6c82e625091a0658c3db4f7a110736421883e1f7
zhouyangye1076 <1076192792@QQ.com>
2023-12-14 17:34:57 +0800

    change some config to fit regvault

--- conf/build.mk
+++ conf/build.mk
@@ -5,7 +5,7 @@ STARSHIP_CORE	?= Rocket
 STARSHIP_FREQ	?= 100
 STARSHIP_TH 	?= starship.fpga.TestHarness
 STARSHIP_TOP	?= starship.fpga.StarshipFPGATop
-STARSHIP_CONFIG	?= starship.fpga.StarshipFPGAConfig
+STARSHIP_CONFIG	?= starship.fpga.StarshipFPGADebugConfig
 
 
 # FPGA Configuration
