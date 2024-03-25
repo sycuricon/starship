5548586b27c07cfebc2ac148da3982332fad48a5
zhouyangye1076 <1076192792@QQ.com>
2024-03-25 13:15:02 +0800

    delete maskrom in peripherals

--- repo/starship/src/main/scala/axi4/Configs.scala
+++ repo/starship/src/main/scala/axi4/Configs.scala
@@ -12,14 +12,14 @@ import freechips.rocketchip.devices.debug._
 import freechips.rocketchip.devices.tilelink._
 
 
-class WithPeripherals extends Config((site, here, up) => {
-  case MaskROMLocated(x) => List(
-    MaskROMParams(BigInt(0x20000L), "StarshipROM")
-  )
-})
+// class WithPeripherals extends Config((site, here, up) => {
+//   case MaskROMLocated(x) => List(
+//     MaskROMParams(BigInt(0x20000L), "StarshipROM")
+//   )
+// })
 
 class StarshipAxi4DebugConfig extends Config(
-  new WithPeripherals ++
+  // new WithPeripherals ++
   new WithJtagDTM ++
   new WithClockGateModel() ++
   new StarshipBaseConfig().alter((site,here,up) => {
