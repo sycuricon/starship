64502a5d1b74db56c363714bfddd3344edc18e6a
Phantom1003 <phantom@zju.edu.cn>
2024-05-08 19:08:52 +0800

    update xs deq signals

--- asic/sim/variant/rob_sync.xiangshan.v
+++ asic/sim/variant/rob_sync.xiangshan.v
@@ -9,31 +9,11 @@
 `define DUT_ROB_DEQ_EN_0        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step.valid
 `define DUT_ROB_DEQ_INST_0      `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step.inst
 `define DUT_ROB_DEQ_PC_0        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step.pc
+`define DUT_ROB_DEQ_DATA_0      `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step.data
 `define DUT_ROB_DEQ_EN_1        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_1.valid
 `define DUT_ROB_DEQ_INST_1      `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_1.inst
 `define DUT_ROB_DEQ_PC_1        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_1.pc
-`define DUT_ROB_DEQ_EN_2        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_2.valid
-`define DUT_ROB_DEQ_INST_2      `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_2.inst
-`define DUT_ROB_DEQ_PC_2        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_2.pc
-`define DUT_ROB_DEQ_EN_3        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_3.valid
-`define DUT_ROB_DEQ_INST_3      `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_3.inst
-`define DUT_ROB_DEQ_PC_3        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_3.pc
-`define DUT_ROB_DEQ_EN_4        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_4.valid
-`define DUT_ROB_DEQ_INST_4      `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_4.inst
-`define DUT_ROB_DEQ_PC_4        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_4.pc
-`define DUT_ROB_DEQ_EN_5        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_5.valid
-`define DUT_ROB_DEQ_INST_5      `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_5.inst
-`define DUT_ROB_DEQ_PC_5        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_5.pc
-`define DUT_ROB_DEQ_EN_6        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_6.valid
-`define DUT_ROB_DEQ_INST_6      `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_6.inst
-`define DUT_ROB_DEQ_PC_6        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_6.pc
-`define DUT_ROB_DEQ_EN_7        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_7.valid
-`define DUT_ROB_DEQ_INST_7      `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_7.inst
-`define DUT_ROB_DEQ_PC_7        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_7.pc
-`define DUT_ROB_DEQ_EN_8        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_8.valid
-`define DUT_ROB_DEQ_INST_8      `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_8.inst
-`define DUT_ROB_DEQ_PC_8        `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_8.pc
-
+`define DUT_ROB_DEQ_DATA_1      `DUT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_1.data
 
 `define VNT_ROB_ENQ_EN_0        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.io_enq_req_0_valid
 `define VNT_ROB_ENQ_INST_0      `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.io_enq_req_0_bits_cf_instr
@@ -44,30 +24,11 @@
 `define VNT_ROB_DEQ_EN_0        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step.valid
 `define VNT_ROB_DEQ_INST_0      `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step.inst
 `define VNT_ROB_DEQ_PC_0        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step.pc
+`define VNT_ROB_DEQ_DATA_0      `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step.data
 `define VNT_ROB_DEQ_EN_1        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_1.valid
 `define VNT_ROB_DEQ_INST_1      `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_1.inst
 `define VNT_ROB_DEQ_PC_1        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_1.pc
-`define VNT_ROB_DEQ_EN_2        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_2.valid
-`define VNT_ROB_DEQ_INST_2      `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_2.inst
-`define VNT_ROB_DEQ_PC_2        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_2.pc
-`define VNT_ROB_DEQ_EN_3        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_3.valid
-`define VNT_ROB_DEQ_INST_3      `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_3.inst
-`define VNT_ROB_DEQ_PC_3        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_3.pc
-`define VNT_ROB_DEQ_EN_4        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_4.valid
-`define VNT_ROB_DEQ_INST_4      `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_4.inst
-`define VNT_ROB_DEQ_PC_4        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_4.pc
-`define VNT_ROB_DEQ_EN_5        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_5.valid
-`define VNT_ROB_DEQ_INST_5      `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_5.inst
-`define VNT_ROB_DEQ_PC_5        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_5.pc
-`define VNT_ROB_DEQ_EN_6        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_6.valid
-`define VNT_ROB_DEQ_INST_6      `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_6.inst
-`define VNT_ROB_DEQ_PC_6        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_6.pc
-`define VNT_ROB_DEQ_EN_7        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_7.valid
-`define VNT_ROB_DEQ_INST_7      `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_7.inst
-`define VNT_ROB_DEQ_PC_7        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_7.pc
-`define VNT_ROB_DEQ_EN_8        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_8.valid
-`define VNT_ROB_DEQ_INST_8      `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_8.inst
-`define VNT_ROB_DEQ_PC_8        `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_8.pc
+`define VNT_ROB_DEQ_DATA_1      `VNT_SOC_TOP.tile_prci_domain.tile_reset_domain_xiangshan_tile.core.core.backend.ctrlBlock.rob.arch_step_1.data
 
 
 always @(posedge clock) begin
@@ -77,13 +38,6 @@ always @(posedge clock) begin
     event_handler(`DUT_ROB_ENQ_EN_1 && `DUT_ROB_ENQ_READY, `DUT_ROB_ENQ_INST_1, "ENQ", 1);
     event_handler(`DUT_ROB_DEQ_EN_0, `DUT_ROB_DEQ_INST_0, "DEQ", 0);
     event_handler(`DUT_ROB_DEQ_EN_1, `DUT_ROB_DEQ_INST_1, "DEQ", 1);
-    event_handler(`DUT_ROB_DEQ_EN_2, `DUT_ROB_DEQ_INST_2, "DEQ", 2);
-    event_handler(`DUT_ROB_DEQ_EN_3, `DUT_ROB_DEQ_INST_3, "DEQ", 3);
-    event_handler(`DUT_ROB_DEQ_EN_4, `DUT_ROB_DEQ_INST_4, "DEQ", 4);
-    event_handler(`DUT_ROB_DEQ_EN_5, `DUT_ROB_DEQ_INST_5, "DEQ", 5);
-    event_handler(`DUT_ROB_DEQ_EN_6, `DUT_ROB_DEQ_INST_6, "DEQ", 6);
-    event_handler(`DUT_ROB_DEQ_EN_7, `DUT_ROB_DEQ_INST_7, "DEQ", 7);
-    event_handler(`DUT_ROB_DEQ_EN_8, `DUT_ROB_DEQ_INST_8, "DEQ", 8);
   end
 end
 
@@ -116,27 +70,13 @@ always @(negedge clock) begin
   if (!sync) begin
     if (
       (`DUT_ROB_DEQ_EN_0 && (`DUT_ROB_DEQ_INST_0 == `INFO_DELAY_END)) ||
-      (`DUT_ROB_DEQ_EN_1 && (`DUT_ROB_DEQ_INST_1 == `INFO_DELAY_END)) ||
-      (`DUT_ROB_DEQ_EN_2 && (`DUT_ROB_DEQ_INST_2 == `INFO_DELAY_END)) ||
-      (`DUT_ROB_DEQ_EN_3 && (`DUT_ROB_DEQ_INST_3 == `INFO_DELAY_END)) ||
-      (`DUT_ROB_DEQ_EN_4 && (`DUT_ROB_DEQ_INST_4 == `INFO_DELAY_END)) ||
-      (`DUT_ROB_DEQ_EN_5 && (`DUT_ROB_DEQ_INST_5 == `INFO_DELAY_END)) ||
-      (`DUT_ROB_DEQ_EN_6 && (`DUT_ROB_DEQ_INST_6 == `INFO_DELAY_END)) ||
-      (`DUT_ROB_DEQ_EN_7 && (`DUT_ROB_DEQ_INST_7 == `INFO_DELAY_END)) ||
-      (`DUT_ROB_DEQ_EN_8 && (`DUT_ROB_DEQ_INST_8 == `INFO_DELAY_END))
+      (`DUT_ROB_DEQ_EN_1 && (`DUT_ROB_DEQ_INST_1 == `INFO_DELAY_END))
     ) begin
       victim_done <= 1;
     end
     if (
       (`VNT_ROB_DEQ_EN_0 && (`VNT_ROB_DEQ_INST_0 == `INFO_DELAY_END)) ||
-      (`VNT_ROB_DEQ_EN_1 && (`VNT_ROB_DEQ_INST_1 == `INFO_DELAY_END)) ||
-      (`VNT_ROB_DEQ_EN_2 && (`VNT_ROB_DEQ_INST_2 == `INFO_DELAY_END)) ||
-      (`VNT_ROB_DEQ_EN_3 && (`VNT_ROB_DEQ_INST_3 == `INFO_DELAY_END)) ||
-      (`VNT_ROB_DEQ_EN_4 && (`VNT_ROB_DEQ_INST_4 == `INFO_DELAY_END)) ||
-      (`VNT_ROB_DEQ_EN_5 && (`VNT_ROB_DEQ_INST_5 == `INFO_DELAY_END)) ||
-      (`VNT_ROB_DEQ_EN_6 && (`VNT_ROB_DEQ_INST_6 == `INFO_DELAY_END)) ||
-      (`VNT_ROB_DEQ_EN_7 && (`VNT_ROB_DEQ_INST_7 == `INFO_DELAY_END)) ||
-      (`VNT_ROB_DEQ_EN_8 && (`VNT_ROB_DEQ_INST_8 == `INFO_DELAY_END))
+      (`VNT_ROB_DEQ_EN_1 && (`VNT_ROB_DEQ_INST_1 == `INFO_DELAY_END))
     ) begin
       victim_done <= 1;
     end
