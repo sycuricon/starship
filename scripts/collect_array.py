f92e40fecb680c3720f20c805cb032c45168c1d1
Phantom1003 <phantom@zju.edu.cn>
2024-08-29 01:40:30 -0400

    update collect array script

--- scripts/collect_array.py
+++ scripts/collect_array.py
@@ -4,6 +4,7 @@ import os
 import argparse
 import re
 from collections import defaultdict
+from itertools import groupby
 
 def parseVecRegister(file):
 
@@ -26,33 +27,55 @@ def parseVecRegister(file):
                     vec_list = defaultdict(set)
 
                     for _, possible_array_list in working_reginfo.items():
-                        for reginfo_pair in possible_array_list:
-                            reg_name = reginfo_pair[0]
-                            reg_type = reginfo_pair[1]
+                        def extract_key(reginfo_pair):
+                            parts = reginfo_pair[0].split('_')
+                            for part in parts:
+                                if part.isdigit() or part == '':
+                                    return '_'.join(parts[:parts.index(part)])
+                            return reginfo_pair[0]
+                        print("[*]", possible_array_list)
+                        for key, group in groupby(possible_array_list, key=extract_key):
+                            group_list = list(group)
+                            print(key, len(group_list), group_list)
 
-                            if reg_type == "mem":
-                                array_list[working_module].add(f"#{reg_name}")
+                            vec_index = set()
                             
-                            else: # is vec
+                            for reginfo in group_list:
+                                reg_name = reginfo[0]
                                 sub_fields = reg_name.split("_")
-                                try:
-                                    item_index = list(map(lambda x: x.isdigit() or len(x) == 0, sub_fields)).index(True)
-                                    bundle_name = "_".join(list(map(lambda x: "" if x.isdigit() else x, sub_fields)))
+                                if reg_name == key:
+                                    vec_index.add('')
+                                for part in sub_fields:
+                                    if part.isdigit() or part == '':
+                                        vec_index.add(part)
+                                        break
+                            
+                            if len(vec_index) <= 1:
+                                print("Remove", key)
+                                continue
 
-                                    # ignore pipeline registers and io buffer
-                                    if sub_fields[0] in ["s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7"] or \
-                                       sub_fields[0] in ["f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7"] or \
-                                       sub_fields[0] in ["io"]:
-                                        continue
 
-                                    # ignore RegNext buffer
-                                    if "REG" in sub_fields:
-                                        continue
+                            for reginfo in group_list:
+                                reg_name = reginfo[0]
+                                sub_fields = reg_name.split("_")
 
-                                    vec_list[bundle_name].add(reg_name)
+                                # # ignore pipeline registers and io buffer
+                                # if sub_fields[0] in ["s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7"] or \
+                                #     sub_fields[0] in ["f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7"] or \
+                                #     sub_fields[0] in ["io"]:
+                                #     continue
 
-                                except ValueError:
+                                # ignore counter
+                                if sub_fields[0] in ["small", "big"]:
                                     continue
+                                
+                                # ignore RegNext buffer
+                                if "REG" in sub_fields:
+                                    continue
+                                
+                                print("Add", reg_name)
+
+                                vec_list[key].add(reg_name)
 
                     for _, vec_set in vec_list.items():
                         if len(vec_set) > 1:
@@ -64,24 +87,20 @@ def parseVecRegister(file):
             if working_module is None:
                 continue
 
-            if (
-                len(line.strip()) == 0
-                or line.strip().split()[0] != "reg"
-                or "@" not in line
-                or "<=" in line
-            ):
-                continue
+            # print(f"[*] Parse line: {line}")
 
-            # print(f"[*] Found reg line: {line[:-1]}")
+            if (len(line.strip()) != 0 and line.strip().split()[0] == "reg" and "@" in line):
+                match_res = re.search(r"reg\s+(\[\d+:\d+\])?\s*([A-Za-z0-9_$]+)\s*(\[\d+:\d+\])?\s*;", line)
+                reg_name = match_res.group(2)
+                reg_type = "vec" if match_res.group(3) is None else "mem"
+                reg_info = line.split("@")[1][1:-2]
 
-            match_res = re.search(r"reg\s+(\[\d+:\d+\])?\s*([A-Za-z0-9_$]+)\s*(\[\d+:\d+\])?\s*;", line)
-            reg_name = match_res.group(2)
-            reg_type = "vec" if match_res.group(3) is None else "mem"
-            reg_info = line.split("@")[1][1:-2]
+                if reg_type == "mem":
+                    continue
 
-            working_reginfo[reg_info].add((reg_name, reg_type))
+                working_reginfo[reg_info].add((reg_name, reg_type))
 
-            # print(f"[*] Found register: `{reg_name}` @ {reg_info}")
+                # print(f"[*] Found {reg_type} under module {working_module}: `{reg_name}` @ {reg_info}")
     
     return array_list
 
@@ -115,6 +134,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-p", "--ignore", nargs="*", type=str, default=[
             "TL", "AXI", "XS_TL", "XS_AXI", 
+            "RenameMapTable",
             "XS_SRAMTemplate", "XS_SyncDataModuleTemplate",
             "XS_FADD_pipe", "XS_BypassNetwork"],
         help="ignored module prefix"
